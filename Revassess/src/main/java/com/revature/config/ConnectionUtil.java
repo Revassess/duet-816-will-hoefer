af593c842048c5d4db1fe23e066e4786621128ea
will-hoefer <will.hoefer@gmail.com>
2020-08-10 17:57:51 -0400

    can't figure out connection

--- Revassess/src/main/java/com/revature/config/ConnectionUtil.java
+++ Revassess/src/main/java/com/revature/config/ConnectionUtil.java
@@ -1,6 +1,10 @@
 package com.revature.config;
 
+import static org.junit.Assert.fail;
+
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  * 
@@ -16,19 +20,35 @@ public class ConnectionUtil {
 	private static ConnectionUtil cu;
 	
 	// add your jdbc url
-	public static final String URL = "";
+	public static final String URL = "jdbc:postgresql://rev-access.cyf2vefafxy0.us-east-1.rds.amazonaws.com/";
 	// add your jdbc username
-	public static final String USERNAME = "";
+	public static final String USERNAME = "whoefer";
 	// add your jdbc password
-	public static final String PASSWORD = "";
+	public static final String PASSWORD = "pumpkins";
 	// name of the created stored procedure in tier 3
 	public static final String TIER_3_PROCEDURE_NAME = "";
 	// name of the created sequence in tier 3
 	public static final String TIER_3_SEQUENCE_NAME = "";
+	private static Connection connection;
 
 	// implement this method to connect to the db and return the connection object
-	public Connection connect(){
-		return null;
+	public Connection connect() throws SQLException{
+		if (connection == null) {
+			try {
+				Class.forName("org.postgresql.Driver");
+			} catch (Exception e) {
+				fail();
+				System.out.println("Could not register driver!");
+				e.printStackTrace();
+			}
+		
+		connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+	}
+		if (connection.isClosed()){
+			System.out.println("Opening new connection...");
+			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		}
+		return connection;
 	}
 
 
